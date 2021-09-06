a1edbf25861d60b45b00ffd5b26acb8f3adfadf6
Adam Wybierala <adam.wybierala@gmail.com>
2021-09-06 16:07:48 +0200

    Ats 96 create model and routes for users
    * CreateUser function calls the gateway
    
    * CreateUser now returns the correct user

---
+++ user/user_calls.go
@@ -0,0 +1,40 @@
+package user
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/asiantripstore/ATS-sdk/models"
+	log "github.com/sirupsen/logrus"
+)
+
+func CreateUser(gatewayHost string, gatewayPort string, user models.User) *models.User {
+	body, err := json.Marshal(user)
+	if err != nil {
+		log.Errorf("Error in Marshal CreateUser")
+		return nil
+	}
+	responseBody := bytes.NewBuffer(body)
+	resp, err := http.Post("http://"+gatewayHost+":"+gatewayPort+"/v1/ats-user/v1/user", "application/json", responseBody)
+	if err != nil {
+		log.Errorf("Error when posting new user, %s", err.Error())
+		return nil
+	}
+	if resp.StatusCode == 200 {
+		log.Info("New user created")
+	}
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("Error when unmarreading response, %s", err.Error())
+		return nil
+	}
+	defer resp.Body.Close()
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		log.Errorf("Error when unmarshalling, %s", err.Error())
+		return nil
+	}
+	return &user
+}
