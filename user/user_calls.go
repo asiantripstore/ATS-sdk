9c75f17428e7b41a68c3085ae72669c68a9bb03b
Adam Wybierala <adam.wybierala@gmail.com>
2021-09-07 09:44:57 +0200

    getUser function

--- user/user_calls.go
+++ user/user_calls.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/asiantripstore/ATS-sdk/models"
 	log "github.com/sirupsen/logrus"
@@ -27,7 +28,32 @@ func CreateUser(gatewayHost string, gatewayPort string, user models.User) *model
 	}
 	body, err = io.ReadAll(resp.Body)
 	if err != nil {
-		log.Errorf("Error when unmarreading response, %s", err.Error())
+		log.Errorf("Error when reading response, %s", err.Error())
+		return nil
+	}
+	defer resp.Body.Close()
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		log.Errorf("Error when unmarshalling, %s", err.Error())
+		return nil
+	}
+	return &user
+}
+
+func GetUser(gatewayHost string, gatewayPort string, id int) *models.User {
+	var user models.User
+	resp, err := http.Get("http://" + gatewayHost + ":" + gatewayPort + "/v1/ats-user/v1/user/" + strconv.Itoa(id))
+	if err != nil {
+		log.Errorf("Error when getting user, %s", err.Error())
+		return nil
+	}
+	if resp.StatusCode != 200 {
+		log.Info("Get user failed")
+		return nil
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("Error when reading response, %s", err.Error())
 		return nil
 	}
 	defer resp.Body.Close()
